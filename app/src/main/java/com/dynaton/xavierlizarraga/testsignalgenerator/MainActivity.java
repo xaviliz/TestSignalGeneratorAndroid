26e151382128a4d8ca63534f504ba43a454a675f
078emil <078emil@gmail.com>
2017-02-12 18:10:12 +0100

    Alert Dialog is now custom with working seekbar Seekbar changes the time in seconds on how long the recording should work --Maybe revise this, to make sure it is not too long or short

--- app/src/main/java/com/dynaton/xavierlizarraga/testsignalgenerator/MainActivity.java
+++ app/src/main/java/com/dynaton/xavierlizarraga/testsignalgenerator/MainActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.media.AudioManager;
@@ -21,6 +22,7 @@ import android.media.SoundPool;
 import android.media.SoundPool.OnLoadCompleteListener;
 import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -33,6 +35,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -40,6 +43,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -72,6 +76,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private final int duration = 20; // seconds
     private final int sampleRate = 44100;
     private final int numSamples = duration * sampleRate;
+    private int recordTimeInMillis;
     private final double sample[] = new double[numSamples];
     private final double freqOfTone = 1000; // hz
     private final byte generatedSnd[] = new byte[2 * numSamples];
@@ -98,6 +103,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private boolean isRecording = false;
     int[] bufferData;
     int bytesRecorded;
+
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -249,20 +256,41 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     }
 
     public void setSignals() {
-        final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create(); //Read Update
-        //LayoutInflater inflater = getLayoutInflater();
-        //alertDialog.setView(inflater.inflate(R.layout.dialog_signal_setttings, null));
-        alertDialog.setTitle("Setting Signals");
-        alertDialog.setMessage("Duration of test recording");
-        final SeekBar seek = new SeekBar(this);
-        seek.setMax(255);
-        seek.setKeyProgressIncrement(1);
-        alertDialog.setView(seek);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.dialog_signal_setttings, (ViewGroup) findViewById(R.id.dialog_view));
+        final TextView textViewDurationSeconds = (TextView) layout.findViewById(R.id.duration_Seconds);
 
-        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        final SeekBar sb = (SeekBar) layout.findViewById(R.id.seekBar);
+        Button btnCancel = (Button) layout.findViewById(R.id.btnCancel);
+        Button btnOk = (Button) layout.findViewById(R.id.btnOk);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this).setView(layout);
+
+        final AlertDialog alertDialog = builder
+                .setCancelable(false)
+                .create();
+
+        final SharedPreferences sharedPreferences = getSharedPreferences("timePreference", Context.MODE_PRIVATE);
+        int durationTime = sharedPreferences.getInt("durationTime", 30);
+        sb.setProgress(durationTime);
+        textViewDurationSeconds.setText(String.valueOf(sb.getProgress()));
+
+        sb.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            int mProgress;
+            int min = 20; //minimum duration
+
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                alertDialog.setMessage("Record time: " + progress + " seconds.");
+                mProgress = progress;
+
+                if (progress < min){
+                    progress = min;
+                }
+                try {
+                    textViewDurationSeconds.setText(String.valueOf(progress));
+                } catch (NullPointerException e){
+                    Log.e(TAG, e.toString());
+                }
             }
 
             @Override
@@ -272,17 +300,32 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        alertDialog.setButton("Continue..", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                // here you can add functions
+                if (mProgress < min){
+                    sb.setProgress(min);
+                }
             }
         });
         alertDialog.show();
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("durationTime", sb.getProgress());
+                editor.apply();
+                Log.d(TAG, "Duration time saved in sec: " + sb.getProgress());
+                alertDialog.dismiss();
+            }
+        });
     }
+
     /*
     // Basic approach to generate a sinewave
     private void playSound(double frequency, int duration) {
@@ -681,6 +724,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onPause() {
         unregisterReceiver(myReceiver);
+        handler.removeCallbacks(timeRunnable);
         super.onPause();
     }
 
@@ -882,7 +926,23 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         }, "AudioRecorder Thread");
 
         recordingThread.start();
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("timePreference",Context.MODE_PRIVATE);
+        int mDurationTime = sharedPreferences.getInt("durationTime", 30);
+        recordTimeInMillis = mDurationTime*1000; //to get millis
+
+        //TODO: handle event on onPause to realese the handler...
+        handler.postDelayed(timeRunnable,recordTimeInMillis);
     }
+
+    private Runnable timeRunnable = new Runnable(){
+        @Override
+        public void run() {
+            Log.i(TAG, "Handler ran after " + recordTimeInMillis + " milliseconds");
+            stopRecording();
+        }
+    };
 
     // Stop recording wave file
     private void stopRecording() {
